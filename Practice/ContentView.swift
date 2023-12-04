86bfe918f8574ee9053d6a9dd9198564405cd2ea
stationAstral <cougarjonez@gmail.com>
2023-12-04 12:21:26 -0800

    1.1

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -9,13 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .foregroundColor(.green)
+                .ignoresSafeArea()
+            VStack {
+                Rectangle()
+                    .foregroundColor(.green)
+                    .frame(height: 75)
+                    .cornerRadius(15)
+                    .ignoresSafeArea()
+                
+                HStack {
+                    // insert a circle
+                    ZStack{
+                        
+                        Rectangle()
+                            .foregroundColor(.red)
+                            .cornerRadius(15)
+                            .ignoresSafeArea()
+                        VStack {
+                            ZStack {
+                                Circle()
+                                    .padding(5)
+                                Circle()
+                                    .colorInvert()
+                                    .frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            }
+                            ZStack {
+                                Rectangle()
+                                Rectangle()
+                                    .colorInvert()
+                                    .frame(width: 30, height: 30)
+                            }
+                            ZStack {
+                                Circle()
+                                    .padding(5)
+                                Circle()
+                                    .colorInvert()
+                                    .frame(width: 30, height: 30)
+                            }
+                            
+                        }
+                    }
+                    //create a vertical line
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 10)
+                        .cornerRadius(15)
+                        .ignoresSafeArea()
+                    
+                    Rectangle()
+                        .cornerRadius(15)
+                        .ignoresSafeArea()
+                    
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 10)
+                        .cornerRadius(15)
+                        .ignoresSafeArea()
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.red)
+                            .cornerRadius(15)
+                            .ignoresSafeArea()
+                        VStack {
+                            ZStack {
+                                Circle()
+                                    .padding(5)
+                                Circle()
+                                    .colorInvert()
+                                    .frame(width: 30, height: 30, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            }
+                            ZStack {
+                                Rectangle()
+                                Rectangle()
+                                    .colorInvert()
+                                    .frame(width: 30, height: 30)
+                            }
+                            ZStack {
+                                Circle()
+                                    .padding(5)
+                                Circle()
+                                    .colorInvert()
+                                    .frame(width: 30, height: 30)
+                            }
+                            
+                        }
+                    }
+                    
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
